9a1f43fd566381063f8ebcf73a38e19d51d40598
phatnm3 <phatnm3@fpt.com.vn>
2020-03-02 17:13:51 +0700

    Form tab body update

--- src/components/FormLoginAndSignUp/index.js
+++ src/components/FormLoginAndSignUp/index.js
@@ -1,52 +1,126 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 import User from "../../apis/user";
 
-import { 
-  makeStyles,
+import {
+  Paper,
   Tabs,
   Tab,
+  Typography,
+  Box,
+  FormControl,
+  InputLabel,
   TextField,
-  Paper
+  OutlinedInput
 } from "@material-ui/core";
 import {
   FormWrapperFixedCenter,
   FormWrapper,
   FormWrapperHeader,
   // MaterialUI makeStyles
-  formTabHeader
-} from './styled';
+  muiTabs,
+  muiTab
+} from "./styled";
 
 // unique key
 const usernameInput = "username";
 const passwordInput = "password";
+
+// TabPanel is tab-body holder
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`form-tabpanel-${index}`}
+      aria-labelledby={`form-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function FormSignIn() {
+  const [values, setValues] = useState({
+    password: "",
+    isShowPassword: false
+  });
+
+  const handleChange = (event, prop) => {
+    setValues({
+      ...values,
+      [prop]: event.target.value
+    });
+  };
+
+  return (
+    <div>
+      <form noValidate autoComplete="off">
+        <TextField fullWidth label="Outlined" variant="outlined" />
+        <FormControl fullWidth variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password-signin">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password-signin"
+            value={values.password}
+            type={values.isShowPassword ? "text" : "password"}
+            onChange={e => handleChange(e, "password")}
+          ></OutlinedInput>
+          <div>{values.password}</div>
+        </FormControl>
+        <TextField fullWidth label="Outlined" variant="outlined" />
+      </form>
+    </div>
+  );
+}
 
 function FormTab() {
   const [value, setValue] = useState(0);
 
   const handleChange = (event, value) => {
     setValue(value);
-  }
+  };
 
-  const classes = formTabHeader();
-  
+  const a11yProps = index => ({
+    id: `form-tab-${index}`,
+    "aria-controls": `form-tabpanel-${index}`
+  });
+
   return (
     <Paper square>
+      {/* Form tab header */}
       <FormWrapperHeader>
         <Tabs
           value={value}
           onChange={handleChange}
           centered={true}
           aria-label="form-tabs-header"
-          classes={{ 
-            root: classes.root,
-            indicator: classes.indicator
-          }}
+          classes={muiTabs()}
         >
-          <Tab label="ALREADY MEMBER ?" />
-          <Tab label="NEW MEMBER ?" />
+          <Tab label="ALREADY MEMBER ?" classes={muiTab()} {...a11yProps(0)} />
+          <Tab label="NEW MEMBER ?" classes={muiTab()} {...a11yProps(1)} />
         </Tabs>
       </FormWrapperHeader>
+      {/* Form tab body */}
+      <div>
+        <TabPanel value={value} index={0}>
+          <FormSignIn />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item 2
+        </TabPanel>
+      </div>
     </Paper>
   );
 }
@@ -80,7 +154,7 @@ function FormLoginAndSignUp() {
     const userSignUp = await user.signUp(userData);
     if (userSignUp) {
       console.log(userSignUp);
-    }  
+    }
   }
 
   return (
--- src/components/FormLoginAndSignUp/styled/index.js
+++ src/components/FormLoginAndSignUp/styled/index.js
@@ -1,21 +1,21 @@
-import styled from 'styled-components';
-import { makeStyles } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
 
 export const FormWrapperFixedCenter = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   position: fixed;
-  top: 0; left: 0;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   overflow: hidden;
-`
+`;
 
 export const FormWrapper = styled.div`
   position: relative;
-`
+`;
 
 export const FormWrapperHeader = styled.div`
   height: 200px;
@@ -23,15 +23,25 @@ export const FormWrapperHeader = styled.div`
   flex-direction: column;
   justify-content: flex-end;
   background-color: #50c1e9;
-`
+`;
 
 // Material css
-export const formTabHeader = makeStyles({
+export const muiTabs = makeStyles(theme => {
+  console.log("theme: ", theme);
+  return {
+    indicator: {
+      backgroundColor: "#75e2cf",
+      boxShadow: "0 0 10px #75e2cf"
+    }
+  };
+});
+
+export const muiTab = makeStyles({
   root: {
-    minHeight: '55px'
+    padding: "18px 36px",
+    background: "#fff"
   },
-  indicator: {
-    backgroundColor: '#75e2cf',
-    boxShadow: '0 0 10px #75e2cf'
+  textColorInherit: {
+    opacity: "1"
   }
 });

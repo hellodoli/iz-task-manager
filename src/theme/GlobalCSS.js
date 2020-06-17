3124bae438d93895a57a1b630ea259625283d9da
phatnm3 <phatnm3@fpt.com.vn>
2020-06-17 19:50:27 +0700

    edit ultil css is important

--- src/theme/GlobalCSS.js
+++ src/theme/GlobalCSS.js
@@ -4,50 +4,50 @@ const utility = (theme) => {
   const typo = theme.typography;
   return {
     '.ml': {
-      marginLeft: typo.pxToRem(10),
+      marginLeft: `${typo.pxToRem(10)}!important`,
       [theme.breakpoints.down('xs')]: {
-        marginLeft: typo.pxToRem(5),
+        marginLeft: `${typo.pxToRem(5)}!important`,
       },
     },
     '.mr': {
-      marginRight: typo.pxToRem(10),
+      marginRight: `${typo.pxToRem(10)}!important`,
       [theme.breakpoints.down('xs')]: {
-        marginRight: typo.pxToRem(5),
+        marginRight: `${typo.pxToRem(5)}!important`,
       },
     },
     '.mt': {
-      marginTop: typo.pxToRem(10),
+      marginTop: `${typo.pxToRem(10)}!important`,
       [theme.breakpoints.down('xs')]: {
-        marginTop: typo.pxToRem(5),
+        marginTop: `${typo.pxToRem(5)}!important`,
       },
     },
     '.mb': {
-      marginBottom: typo.pxToRem(10),
+      marginBottom: `${typo.pxToRem(10)}!important`,
       [theme.breakpoints.down('xs')]: {
-        marginBottom: typo.pxToRem(5),
+        marginBottom: `${typo.pxToRem(5)}!important`,
       },
     },
     '.font-weight-bold': {
-      fontWeight: 'bold',
+      fontWeight: 'bold!important',
     },
     '.font-style-italic': {
-      fontStyle: 'italic',
+      fontStyle: 'italic!important',
     },
     '.text-right': {
-      textAlign: 'right',
+      textAlign: 'right!important',
     },
     '.text-uppercase': {
-      textTransform: 'uppercase',
+      textTransform: 'uppercase!important',
     },
     '.text-mute': {
-      opacity: '.5',
+      opacity: '.5!important',
     },
     '.divider-line': {
-      borderBottom: `1px solid ${theme.palette.divider}`,
-      backgroundClip: 'padding-box',
+      borderBottom: `1px solid ${theme.palette.divider}!important`,
+      backgroundClip: 'padding-box!important',
     },
     '.cursor-pointer': {
-      cursor: 'pointer',
+      cursor: 'pointer!important',
     },
   };
 };
